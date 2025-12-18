5875c4e248c596d8d8dd7e03ac4e1b471277efd9
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-12-18 08:16:24 +0000

    Add styling to ProfileSearch component

--- src/components/ProfileSearch.jsx
+++ src/components/ProfileSearch.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import useAPI from "../hooks/useAPI";
+import styled from "styled-components";
 
 export default function ProfileSearch() {
     const { searchProfiles } = useAPI();
@@ -19,22 +20,65 @@ export default function ProfileSearch() {
     }, [query]);
 
     return (
-        <div>
-            <input
+        <Container>
+            <SearchInput
                 type="text"
-                placeholder="Search by name or username..."
+                placeholder="Search users..."
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
             />
 
-            <div>
+            <ResultsContainer>
                 {results &&
                     results.map((user) => (
-                        <div key={user.id}>
+                        <FoundProfile key={user.id}>
                             {user.name} (@{user.username})
-                        </div>
+                        </FoundProfile>
                     ))}
-            </div>
-        </div>
+            </ResultsContainer>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    border-radius: 5px;
+    padding: 0.5rem;
+    background-color: #3d3d3d;
+`;
+
+const SearchInput = styled.input`
+    background-color: #252525;
+    border: none;
+    border-radius: 5px;
+    padding: 0.5rem;
+    width: 200px;
+    color: white;
+
+    &::placeholder {
+        color: #afafaf;
+    }
+`;
+
+const ResultsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+`;
+
+const FoundProfile = styled.div`
+    padding: 0.5rem;
+    border: 1px solid #686909;
+    border-radius: 5px;
+    background-color: #252525;
+
+    &:hover {
+        background-color: #1b611e;
+        cursor: pointer;
+    }
+`;
